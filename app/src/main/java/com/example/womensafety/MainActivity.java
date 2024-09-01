3d82fa4d75f3d54f6018df45d105634ed47bbaf4
kanishk raj <kanishkraj600@gmail.com>
2024-09-02 00:12:28 +0530

    women safety

--- app/src/main/java/com/example/womensafety/MainActivity.java
+++ app/src/main/java/com/example/womensafety/MainActivity.java
@@ -19,26 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.home_page);
+        setContentView(R.layout.alerts_table);
 
         // Set padding to account for system bars
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.alertsbody), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
         // Initialize drawer layout and toggle button
-     drawerLayout = findViewById(R.id.main);
-        imageButtonToggle = findViewById(R.id.imageButtonToggle);
-
-        // Set click listener for the toggle button
-        imageButtonToggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.open();
-            }
-        });
+//     drawerLayout = findViewById(R.id.main);
+//        imageButtonToggle = findViewById(R.id.imageButtonToggle);
+//
+//        // Set click listener for the toggle button
+//        imageButtonToggle.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                drawerLayout.open();
+//            }
+//        });
     }
 
 }
